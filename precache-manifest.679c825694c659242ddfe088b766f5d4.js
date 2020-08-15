afcb122adc49bc3b5b6968a45671d73b868281d6
Rita Gaherwar <rita@ranium.in>
2020-08-15 17:14:39 +0530

    Updates

---
+++ precache-manifest.679c825694c659242ddfe088b766f5d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6144989aea76590667de77fc6cef9796",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e63af08e9cb44b8198e1",
+    "url": "/static/css/main.77b4d543.chunk.css"
+  },
+  {
+    "revision": "ddfc98fd1fb7cc1baeab",
+    "url": "/static/js/2.fcb60162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fcb60162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63af08e9cb44b8198e1",
+    "url": "/static/js/main.7324c16f.chunk.js"
+  },
+  {
+    "revision": "25fe1780fd26c1e21c1e",
+    "url": "/static/js/runtime-main.78dac5f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
